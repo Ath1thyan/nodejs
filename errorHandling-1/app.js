716fef69de5db6101a64b1a351d1d59dd54956ae
Ath1thyan <athitasks@gmail.com>
2024-11-22 21:35:03 +0530

    error-handling

--- errorHandling-1/app.js
+++ errorHandling-1/app.js
@@ -1,35 +1,28 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
+const { tryCatch } = require("./utils/tryCatch");
 
 const app = express();
 
 const getUser = () => undefined;
 
-app.get("/test", async (req, res) => {
-    try {
-        const user = getUser();
-        if (!user) {
-            throw new Error("User not found");
-        }
-    } catch (error) {
-        console.log(error);
-        next
-    }
-    return res.status(200).json({ success: true });
-});
+app.get("/test", tryCatch(
+    async (req, res, next) => {
+            const user = getUser();
+            if (!user) {
+                throw new Error("User not found");
+            }
+        return res.status(200).json({ success: true });
+    })
+);
 
 
-app.post("/login", async (req, res) => {
-    try {
-        const user = getUser();
-        if (!user) {
-            throw new Error("User not found");
-        }
-    } catch (error) {
-        console.log(error);
-        return res.status(400).send(error.message);
-    }
-    return res.status(200).json({ success: true });
-});
+
+app.post("/login", tryCatch(async (req, res) => {
+
+}));
+
+app.use(errorHandler);
 
 app.listen(4000, () => {
     console.log("Server listening on port 4000");
